d927c2f458358e0f40de181de62409d9af05c76c
Geroid64 <gerodin64@gmail.com>
2023-01-12 00:18:31 +0100

    Trying to make simple enemy work

--- Assets/_Game/Scripts/EnemySimple.cs
+++ Assets/_Game/Scripts/EnemySimple.cs
@@ -12,9 +12,15 @@ public class EnemySimple : MonoBehaviour
     public LayerMask mask;
     public Vector3 vector = new Vector3(0, 0, 0);
     public Vector3 dest;
-    public bool PlayerNotFound = true;
+
+    private bool coroutine_start = true;
+    private bool coroutine_attack = true;
     private NavMeshAgent navMesh;
-    private Coroutine routine=null;
+    private Coroutine routine = null;
+    private Coroutine routine_attack = null;
+
+    LineRenderer lineee;
+
     private void Awake()
     {
         navMesh = GetComponentInParent<NavMeshAgent>();
@@ -23,54 +29,99 @@ public class EnemySimple : MonoBehaviour
     private void Start()
     {
         routine = StartCoroutine(Scan(dest));
-
+        lineee = GetComponent<LineRenderer>();
     }
-    private void Update()
+
+    private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            coroutine_attack = false;
 
+            coroutine_start = false;
+        }
+    }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            routine = StartCoroutine(Scan(dest));
+
+            if (routine_attack != null)
+            {
+                StopCoroutine(routine_attack);
+            }
+        }
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            PlayerNotFound = false;
-            Debug.Log("WWWW");
             if (Physics.Raycast(transform.position+vector, (other.transform.position - transform.position).normalized, out raycast, Mathf.Infinity,~mask))
             {
+                //Debug.Log("HELP++CZY TRAFIA: " + Physics.Raycast(transform.position + vector, (other.transform.position - transform.position).normalized, out raycast, Mathf.Infinity, ~mask));
                 if (raycast.collider.gameObject.CompareTag("Player")|| raycast.collider.gameObject.CompareTag("PlayerSee"))
                 {
-                    
-                    //StopCoroutine(routine);
-                    navMesh.destination = other.gameObject.transform.position;
-                    Debug.Log("WWWWWWWIDAC" + raycast.collider.gameObject.name);
-                    Debug.DrawRay(transform.position+vector, (other.transform.position - transform.position), Color.black);
-                }
+                    Debug.Log("HELP==WIDZI");
+                    if(coroutine_attack==false)
+                    {
+                        coroutine_attack = true;
+                        routine_attack = StartCoroutine(Attack(other.GetComponent<ScriptHealth>()));
+                    }
 
+                    navMesh.destination = other.gameObject.transform.position;
+
+                    transform.parent.transform.rotation= Quaternion.LookRotation(other.transform.position - transform.position, Vector3.up);
+                }
                 else
                 {
-                    Debug.Log("WWWWWWW===NIEEEEE--IDAC" + raycast.collider.gameObject.name);
+                    Debug.Log("HELP==NIE_WIDZI");
                     Debug.DrawRay(transform.position+vector, (other.transform.position - transform.position), Color.white);
-                    PlayerNotFound = true;
-                    //routine = StartCoroutine(Scan(dest));
+                    if (routine_attack != null)
+                    {
+                        StopCoroutine(routine_attack);
+                    }
+                    if (coroutine_start == false)
+                    {
+                        coroutine_start = true;
+                        routine = StartCoroutine(Scan(dest));
+                    }
+
                 }
             }
-
-
                 
         }
     }
 
     IEnumerator Scan(Vector3 dest)
     {
-        while(true)
+        while(coroutine_start)
         {
-            dest = RandomNavSphere(transform.position, 10);
-            //dest = new Vector3(Random.Range(0, 10), 0, Random.Range(0, 10));
-            Debug.Log("EEEEEEEHHHHH");
+            dest = RandomPointNavMesh(transform.position, 10);
             navMesh.destination = dest;
-            yield return new WaitForSeconds(3);
+            yield return new WaitForSeconds(Random.Range(2,3));
+        }
+    }
+
+    IEnumerator Attack(ScriptHealth player)
+    {
+        Debug.Log("HELPP"+coroutine_attack);
+        while(coroutine_attack)
+        {
+            lineee.SetPosition(0, transform.position);
+            lineee.SetPosition(1, player.transform.position);
+
+            lineee.enabled = true;
+            yield return new WaitForSeconds(0.1f);
+            lineee.enabled = false;
+
+
+            player.Suffer("damage", 5);
+            yield return new WaitForSeconds(1);
+
         }
 
     }
@@ -84,21 +135,5 @@ public class EnemySimple : MonoBehaviour
         return is_nav.position;
     }
 
-        public void EnemyMood(string state)
-    {
-        switch (state)
-        {
-            case "passive":
-                break;
-            case "explore":
-                break;
-            case "agitated":
-                break;
-            case "agressive":
-                break;
-            case "scared":
-                break;
-        }
-    }
 
 }
