ec2d47e90d33f116040726d7d06e1d0d7ebe1a4c
Geroid64 <gerodin64@gmail.com>
2023-01-30 23:59:36 +0100

    Main menu has worikng credits button

--- Assets/MainMenuScript.cs
+++ Assets/MainMenuScript.cs
@@ -6,53 +6,49 @@ using UnityEngine.SceneManagement;
 public class MainMenuScript : MonoBehaviour
 {
     public UIDocument ui_doc;
-    public UIDocument ui_credits;
     Button credits_button, settings_button, continue_button, start_button;
     Button return_button;
+    VisualElement return_background;
     //names
     //    CreditsButton
     //    SettingsButton
     //    ContinueButton
     //    StartButton
+    //    ReturnButton
 
     void OnEnable()
     {
-
-        
-        ui_credits.rootVisualElement.style.display = DisplayStyle.None;
         credits_button = ui_doc.rootVisualElement.Q("CreditsButton") as Button;
         settings_button = ui_doc.rootVisualElement.Q("SettingsButton") as Button;
         continue_button = ui_doc.rootVisualElement.Q("ContinueButton") as Button;
         start_button = ui_doc.rootVisualElement.Q("StartButton") as Button;
+        return_button = ui_doc.rootVisualElement.Q("ReturnButton") as Button;
+
+        return_background = ui_doc.rootVisualElement.Q("ReturnButtonBackground") as VisualElement;
+
+        return_button.clicked += () => HideCredits();
         credits_button.clicked += () => ShowCredits();
         settings_button.clicked += () => ShowSettings();
         continue_button.clicked += () => ContinueGame();
         start_button.clicked += () => StartNewGame();
+ 
+        return_background.SetEnabled(false);
+        return_button.SetEnabled(false);
+        return_background.visible = false;
+
     }
 
     public void ShowCredits()
     {
-        ui_doc.rootVisualElement.style.display = DisplayStyle.None;
-        ui_credits.rootVisualElement.style.display = DisplayStyle.Flex;
-        return_button = ui_doc.rootVisualElement.Q("ReturnButton") as Button;
-        return_button.clicked += () => HideCredits();
-
-
-
-
-
- 
-
-        //button 1
-        //scrolling with textmesh
-        //button 2
+        return_background.visible = true;
+        return_background.SetEnabled(true);
+        return_button.SetEnabled(true);
     }
     public void HideCredits()
     {
-        Debug.Log("MMM");
-        ui_credits.rootVisualElement.style.display = DisplayStyle.None;
-        ui_doc.rootVisualElement.style.display = DisplayStyle.Flex;
-
+        return_background.SetEnabled(false);
+        return_button.SetEnabled(false);
+        return_background.visible = false;
     }
     public void ShowSettings()
     {
