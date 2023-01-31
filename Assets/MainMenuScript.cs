8d43d063683f538f98f4c953656e05c0bdb51a02
Geroid64 <gerodin64@gmail.com>
2023-01-31 13:45:42 +0100

    Credits are working

--- Assets/MainMenuScript.cs
+++ Assets/MainMenuScript.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
+using TMPro;
 public class MainMenuScript : MonoBehaviour
 {
     public UIDocument ui_doc;
     Button credits_button, settings_button, continue_button, start_button;
     Button return_button;
     VisualElement return_background;
+    public TMP_Text credits_text;
+    public GameObject credits_game_object;
+    Coroutine routine;
+    Vector3 credits_initial;
     //names
     //    CreditsButton
     //    SettingsButton
@@ -18,6 +23,7 @@ public class MainMenuScript : MonoBehaviour
 
     void OnEnable()
     {
+        credits_initial = credits_text.transform.position;
         credits_button = ui_doc.rootVisualElement.Q("CreditsButton") as Button;
         settings_button = ui_doc.rootVisualElement.Q("SettingsButton") as Button;
         continue_button = ui_doc.rootVisualElement.Q("ContinueButton") as Button;
@@ -31,7 +37,7 @@ public class MainMenuScript : MonoBehaviour
         settings_button.clicked += () => ShowSettings();
         continue_button.clicked += () => ContinueGame();
         start_button.clicked += () => StartNewGame();
- 
+        credits_game_object.SetActive(false);
         return_background.SetEnabled(false);
         return_button.SetEnabled(false);
         return_background.visible = false;
@@ -43,12 +49,17 @@ public class MainMenuScript : MonoBehaviour
         return_background.visible = true;
         return_background.SetEnabled(true);
         return_button.SetEnabled(true);
+        credits_game_object.SetActive(true);
+        routine = StartCoroutine(MoveCredits(true));
     }
     public void HideCredits()
     {
         return_background.SetEnabled(false);
         return_button.SetEnabled(false);
         return_background.visible = false;
+        credits_game_object.SetActive(false);
+        StopCoroutine(routine);
+        credits_text.transform.position = credits_initial;
     }
     public void ShowSettings()
     {
@@ -63,5 +74,14 @@ public class MainMenuScript : MonoBehaviour
     public void StartNewGame()
     {
         SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    IEnumerator MoveCredits(bool is_scrolling)
+    {
+        while(true)
+        {
+            credits_text.transform.position += Vector3.up * Time.deltaTime;
+            yield return null;
+        }
     }
 }
