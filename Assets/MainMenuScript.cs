3e9a19af606f0a2e45bdcc79ef1b902fa2697f33
Geroid64 <gerodin64@gmail.com>
2023-01-31 13:56:57 +0100

    Quitting the game from menu done

--- Assets/MainMenuScript.cs
+++ Assets/MainMenuScript.cs
@@ -7,7 +7,7 @@ using TMPro;
 public class MainMenuScript : MonoBehaviour
 {
     public UIDocument ui_doc;
-    Button credits_button, settings_button, continue_button, start_button;
+    Button credits_button, settings_button, continue_button, start_button, quit_button;
     Button return_button;
     VisualElement return_background;
     public TMP_Text credits_text;
@@ -20,6 +20,7 @@ public class MainMenuScript : MonoBehaviour
     //    ContinueButton
     //    StartButton
     //    ReturnButton
+    //    QuitButton
 
     void OnEnable()
     {
@@ -29,6 +30,7 @@ public class MainMenuScript : MonoBehaviour
         continue_button = ui_doc.rootVisualElement.Q("ContinueButton") as Button;
         start_button = ui_doc.rootVisualElement.Q("StartButton") as Button;
         return_button = ui_doc.rootVisualElement.Q("ReturnButton") as Button;
+        quit_button = ui_doc.rootVisualElement.Q("QuitButton") as Button;
 
         return_background = ui_doc.rootVisualElement.Q("ReturnButtonBackground") as VisualElement;
 
@@ -37,6 +39,7 @@ public class MainMenuScript : MonoBehaviour
         settings_button.clicked += () => ShowSettings();
         continue_button.clicked += () => ContinueGame();
         start_button.clicked += () => StartNewGame();
+        quit_button.clicked += () => Application.Quit();
         credits_game_object.SetActive(false);
         return_background.SetEnabled(false);
         return_button.SetEnabled(false);
