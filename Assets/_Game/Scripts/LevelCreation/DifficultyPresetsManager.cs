9a64199a2788e8a83948a220fcb9b087af525615
Geroid64 <gerodin64@gmail.com>
2023-02-12 22:03:31 +0100

    Changed presets

--- Assets/_Game/Scripts/LevelCreation/DifficultyPresetsManager.cs
+++ Assets/_Game/Scripts/LevelCreation/DifficultyPresetsManager.cs
@@ -35,38 +35,43 @@ public class DifficultyPresetsManager : MonoBehaviour
         switch (difficulty[difficulty_index])
         {
             case "easy":
-                max_enemy = 10;
+                max_enemy = 30;
                 max_health = 15;
-                max_money = 5;
+                max_money = 15;
                 break;
             case "normal":
-                max_enemy = 25;
-                max_health = 10;
-                max_money = 7;
+                max_enemy = 50;
+                max_health = 20;
+                max_money = 20;
                 break;
             case "hard":
-                max_enemy = 60;
-                max_health = 5;
-                max_money = 13;
+                max_enemy = 80;
+                max_health = 10;
+                max_money = 35;
                 break;
         }
 
         switch (resource_spread[resource_spread_index])
         {
             case "stone":
-                max_stone = 20;
-                max_wood = 4;
-                max_metal = 8;
-                break;
-            case "wood":
-                max_stone = 6;
-                max_wood = 25;
-                max_metal = 3;
-                break;
-            case "equal":
-                max_stone = 10;
+                max_stone = 35;
                 max_wood = 10;
                 max_metal = 10;
+                break;
+            case "wood":
+                max_stone = 10;
+                max_wood = 25;
+                max_metal = 10;
+                break;
+            case "metal":
+                max_stone = 10;
+                max_wood = 13;
+                max_metal = 30;
+                break;
+            case "equal":
+                max_stone = 20;
+                max_wood = 20;
+                max_metal = 20;
                 break;
         }
     }
